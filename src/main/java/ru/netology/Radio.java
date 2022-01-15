fda7a01ef657be4db163b963a5295266e3581b14
Padyaa <zehopa@mail.ru>
2022-01-15 11:00:54 +0300

    revision

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -9,31 +9,39 @@ public class Radio {
         return currentStation;
     }
 
-    public void setCurrentStation(int currentStation) {
-        if (currentStation < 0) {
-            this.currentStation = 9;
-            return;
-        }
+    public void setNextStation(int currentStation) {
         if (currentStation > 9) {
             this.currentStation = 0;
             return;
         }
-        this.currentStation = currentStation;
+        this.currentStation = currentStation + 1;
+    }
+
+    public void setPreviousStation(int currentStation) {
+        if (currentStation < 0) {
+            this.currentStation = 9;
+            return;
+        }
+        this.currentStation = currentStation - 1;
     }
 
     public int getCurrentVolume() {
         return currentVolume;
     }
 
-    public void setCurrentVolume(int currentVolume) {
+    public void setRaiseTheVolume(int currentVolume) {
         if (currentVolume >= 10) {
             this.currentVolume = 10;
             return;
         }
+        this.currentVolume = currentVolume + 1;
+    }
+
+    public void setLowerTheVolume(int currentVolume) {
         if (currentVolume <= 0) {
             this.currentVolume = 0;
             return;
         }
-            this.currentVolume = currentVolume + 1;
+        this.currentVolume = currentVolume - 1;
     }
 }
--- src/test/java/ru/netology/RadioTest.java
+++ src/test/java/ru/netology/RadioTest.java
@@ -1,6 +1,7 @@
 package ru.netology;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
 class RadioTest {
@@ -8,42 +9,70 @@ class RadioTest {
     @Test
     public void shouldSetStationForTheLowerLimit() {
         Radio radio = new Radio();
-        radio.setCurrentStation(-1);
+        radio.setPreviousStation(-1);
         assertEquals(9, radio.getCurrentStation());
     }
 
     @Test
     public void shouldSetStationForTheUpperLimit() {
         Radio radio = new Radio();
-        radio.setCurrentStation(11);
+        radio.setNextStation(11);
         assertEquals(0, radio.getCurrentStation());
     }
 
     @Test
-    public void shouldSetTrueStation() {
+    public void shouldSetNextStation() {
         Radio radio = new Radio();
-        radio.setCurrentStation(5);
-        assertEquals(5, radio.getCurrentStation());
+        radio.setNextStation(5);
+        assertEquals(6, radio.getCurrentStation());
     }
 
     @Test
-    public void shouldSetTrueVolume() {
+    public void shouldSetPreviousStation() {
         Radio radio = new Radio();
-        radio.setCurrentVolume(3);
+        radio.setPreviousStation(5);
+        assertEquals(4, radio.getCurrentStation());
+    }
+
+    @Test
+    public void shouldSetNextVolume() {
+        Radio radio = new Radio();
+        radio.setRaiseTheVolume(3);
+        assertEquals(4, radio.getCurrentVolume());
+    }
+
+    @Test
+    public void shouldSetPreviousVolume() {
+        Radio radio = new Radio();
+        radio.setLowerTheVolume(5);
         assertEquals(4, radio.getCurrentVolume());
     }
 
     @Test
     public void shouldSetOverMaxVolume() {
         Radio radio = new Radio();
-        radio.setCurrentVolume(11);
+        radio.setRaiseTheVolume(11);
         assertEquals(10, radio.getCurrentVolume());
     }
 
     @Test
     public void shouldSetUnderMinVolume() {
         Radio radio = new Radio();
-        radio.setCurrentVolume(-1);
+        radio.setLowerTheVolume(-1);
         assertEquals(0, radio.getCurrentVolume());
+    }
+
+    @Test
+    public void shouldSetMinVolumeLimit() {
+        Radio radio = new Radio();
+        radio.setLowerTheVolume(0);
+        assertEquals(0, radio.getCurrentVolume());
+    }
+
+    @Test
+    public void shouldSetMaxVolumeLimit() {
+        Radio radio = new Radio();
+        radio.setRaiseTheVolume(10);
+        assertEquals(10, radio.getCurrentVolume());
     }
 }
